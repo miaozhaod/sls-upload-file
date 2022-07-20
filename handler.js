c0f6f0285d3d8587545e6bfdaa7e01fef19819ae
Miao Zhao <miao_zhao@yahoo.com>
2022-07-20 19:23:57 +1000

    Update to use `lambda-multipart-parser`

--- handler.js
+++ handler.js
@@ -1,20 +1,25 @@
 const AWS = require("aws-sdk");
 const parseMultipart = require("parse-multipart");
+const parser = require("lambda-multipart-parser");
 
 const BUCKET = process.env.BUCKET;
 const s3 = new AWS.S3();
 
 module.exports.uploadFile = async event => {
+  console.log("lambda uploadFile event ... ", event);
+  console.log("lambda uploadFile event string ... ", JSON.stringify(event));
+  const result = await parser.parse(event);
+  console.log("result ... ", result);
+  const { filename, content } = result.files[0];
+  console.log("filename ... ", filename);
+  console.log("content ... ", content);
   try {
-    const { filename, data } = extractFile(event);
-    console.log("filenane1: ", filename);
-    console.log("data1: ", data);
     await s3
       .putObject({
         Bucket: BUCKET,
         Key: filename,
         ACL: "public-read",
-        Body: data,
+        Body: content,
       })
       .promise();
 
@@ -33,18 +38,20 @@ module.exports.uploadFile = async event => {
 };
 
 function extractFile(event) {
-  console.log("event.headers", event.headers["content-type"]);
-  console.log("event.body", event.body);
+  console.log(
+    "extractFile event.headers.content-type ...",
+    event.headers["content-type"]
+  );
+  console.log("extractFile event.body ...", event.body);
   const boundary = parseMultipart.getBoundary(event.headers["content-type"]);
-  console.log("boundary", boundary);
   const parts = parseMultipart.Parse(
     Buffer.from(event.body, "base64"),
     boundary
   );
-  console.log("parts", parts);
+  console.log("extractFile parts ... ", parts);
   const [{ filename, data }] = parts;
-  console.log("filenane: ", filename);
-  console.log("data: ", data);
+  console.log("extractFile parts:filenane... ", filename);
+  console.log("extractFile parts:data ... ", data);
 
   return {
     filename,
