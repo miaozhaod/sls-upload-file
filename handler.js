fd0188d9d96c32156a7cbfefca9b1cb041d8159d
Miao Zhao <miao_zhao@yahoo.com>
2022-07-20 20:33:35 +1000

    Remove unused lines

--- handler.js
+++ handler.js
@@ -6,13 +6,11 @@ const BUCKET = process.env.BUCKET;
 const s3 = new AWS.S3();
 
 module.exports.uploadFile = async event => {
-  console.log("lambda uploadFile event ... ", event);
   console.log("lambda uploadFile event string ... ", JSON.stringify(event));
   const result = await parser.parse(event);
   console.log("result ... ", result);
   const { filename, content } = result.files[0];
-  console.log("filename ... ", filename);
-  console.log("content ... ", content);
+
   try {
     await s3
       .putObject({
@@ -37,24 +35,3 @@ module.exports.uploadFile = async event => {
   }
 };
 
-function extractFile(event) {
-  console.log(
-    "extractFile event.headers.content-type ...",
-    event.headers["content-type"]
-  );
-  console.log("extractFile event.body ...", event.body);
-  const boundary = parseMultipart.getBoundary(event.headers["content-type"]);
-  const parts = parseMultipart.Parse(
-    Buffer.from(event.body, "base64"),
-    boundary
-  );
-  console.log("extractFile parts ... ", parts);
-  const [{ filename, data }] = parts;
-  console.log("extractFile parts:filenane... ", filename);
-  console.log("extractFile parts:data ... ", data);
-
-  return {
-    filename,
-    data,
-  };
-}
